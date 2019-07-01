85dd10742a48cad8f539d5574dd16065973251f2
marceloabreu-escola <52409440+marceloabreu-escola@users.noreply.github.com>
2019-07-01 07:21:55 -0700

    Add files via upload

---
+++ PokedexiOS/PokedexMarcelo/PokedexMarcelo/ListarPokemonViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListarPokemonViewController.swift
+//  PokedexMarcelo
+//
+//  Created by Marcelo Abreu on 27/06/2019.
+//  Copyright © 2019 Marcelo Abreu. All rights reserved.
+//
+
+import UIKit
+
+class ListarPokemonViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ListarPokemonView has loaded!")
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
